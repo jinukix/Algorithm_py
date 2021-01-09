946a42b6a1bd0e3d1ae8ebabb49585aded305b06
장진욱 <wfo3977@gmail.com>
2021-01-09 13:12:32 +0900

    21.01.09'

--- zxc.py
+++ zxc.py
@@ -1,1 +1,15 @@
-x
+"""
+
+shfit opt f : 코드서식 맞추기
+"""
+
+
+def a(a, b, c, d, e):
+    ygf =1
+    ygf+=1
+    ygf-=1
+    ygf = c+1
+    b = ygf+ygf
+
+
+    print(1)
---
+++ 구현/[프로그래머스]직사각형 별찍기.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().strip().split(' '))
+
+star = "*" * a
+
+for i in range(0,b):
+    print(star)
