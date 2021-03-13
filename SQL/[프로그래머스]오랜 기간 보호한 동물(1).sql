3b53c8fa491ca2d9b00b85b33cfb829225acc49e
장진욱 <wfo3977@gmail.com>
2021-03-13 21:18:36 +0900

    21.03.13

---
+++ SQL/[프로그래머스]오랜 기간 보호한 동물(1).sql
@@ -0,0 +1,9 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59044
+
+SELECT INS.NAME, INS.DATETIME
+FROM ANIMAL_INS AS INS
+LEFT JOIN ANIMAL_OUTS AS OUTS
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+where outs.animal_id is null
+ORDER BY INS.DATETIME
+LIMIT 3
