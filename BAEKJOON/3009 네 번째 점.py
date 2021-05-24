ce50f6829d9c197fbb7acba4c44ede01c8748916
Jinuk <wfo3977@gmail.com>
2021-05-24 23:45:23 +0900

    21.05.24

--- BAEKJOON/3009 네 번째 점.py
+++ BAEKJOON/3009 네 번째 점.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def read():
     return sys.stdin.readline().strip()
 
@@ -7,12 +8,12 @@ def read():
 x_list = []
 y_list = []
 
-for i in range(3):    
-    x, y = map(int, read().split())
+x, y = 0, 0
 
+for i in range(3):
+    x, y = map(int, read().split())
     x_list.append(x)
     y_list.append(y)
-    
 
 for i in range(3):
     if x_list.count(x_list[i]) == 1:
@@ -20,5 +21,5 @@ for i in range(3):
 
     if y_list.count(y_list[i]) == 1:
         y = y_list[i]
-    
+
 print(x, y)
